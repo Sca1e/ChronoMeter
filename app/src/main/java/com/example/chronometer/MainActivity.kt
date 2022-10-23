23ee7ad01302e5fc646e99cc040169072e0c8e96
Your Name <you@example.com>
2022-10-22 19:45:19 -1200

    last update the code

--- app/src/main/java/com/example/chronometer/MainActivity.kt
+++ app/src/main/java/com/example/chronometer/MainActivity.kt
@@ -18,7 +18,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//
         buttonStartPause = findViewById(R.id.startPause)
         buttonReset = findViewById(R.id.reset)
         chronometer = findViewById(R.id.chronometer)
